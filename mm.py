627cee0d00462d66d9a8a4c024beec9cb54b21de
Kyle Burkett <kyle.burkett@wwinc.com>
2021-02-26 11:52:02 -0400

    push preliminary poc

--- mm.py
+++ mm.py
@@ -2,41 +2,14 @@ import sys
 import time
 import win32api
 
-if (len(sys.argv) < 4):
-	print "Usage: python mousemove.py dx dy speed"
-	sys.exit()
-
-current = win32api.GetCursorPos()
-cx = sx = current[0]
-cy = sy = current[1]
-
-mx = int(sys.argv[1])
-my = int(sys.argv[2])
-vx = vy = int(sys.argv[3])
-
-print "Moving", mx, my, "with", vx, "pixels per second"
-print "Press 'q' to quit"
-
-last = time.time()
-
 while(True):
-	if win32api.GetAsyncKeyState(ord('Q')):
-		sys.exit()
-		
-	current = time.time()
-	tick = current - last
-	last = current
-	
-	if mx > 0:
-		cx += vx * tick;
-		if cx > mx + sx or cx < sx:
-			vx = -vx;
-			cx = max( sx, min( mx + sx, cx ) )
-	if( my > 0 ):
-		cy += vy * tick;
-		if cy > my + sy or cy < sy:
-			vy = -vy;
-			cy = max( sy, min( my + sy, cy ) )
-	
-	win32api.SetCursorPos((int(cx),int(cy)))
-	time.sleep(0.001)
+	current = win32api.GetCursorPos()
+	cx = current[0]
+	cy = current[1]
+	if( (int(time.time())%2) == 0):
+		mx = cx+1
+	else:
+		mx = cx-1
+	my = cy
+	win32api.SetCursorPos((int(mx),int(my)))
+	time.sleep(11)
---
+++ readme.md
@@ -0,0 +1,13 @@
+### Steps to run the script and keep that windows pc alive
+
+- Clone repository
+
+- Install python
+
+- Install pywin32 extension
+
+  - `pip install -r requirements.txt`
+
+- Run script
+  
+  - `py mm.py`
